9d3b1ad0113042d79924fa7a48ee7cc695cb2551
Gilberto Stankiewicz <kiewic@gmail.com>
2016-12-27 13:48:51 -0600

    Animate multiple vertical lines and add colors.

--- app/src/main/java/com/mocoyoyo/rainbowunicorn/RainbowActivity.java
+++ app/src/main/java/com/mocoyoyo/rainbowunicorn/RainbowActivity.java
@@ -1,24 +1,34 @@
 package com.mocoyoyo.rainbowunicorn;
 
+import android.animation.Animator;
+import android.animation.LayoutTransition;
 import android.animation.ValueAnimator;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.annotation.ColorInt;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class RainbowActivity extends AppCompatActivity {
+    private int canvasColor = Color.MAGENTA;
+    private float positionY;
     private float progress;
     private Bitmap bitmap;
     private Canvas canvas;
     private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,38 +45,92 @@ public class RainbowActivity extends AppCompatActivity {
 
                 Log.d("foo", "drawing!");
                 bitmap = Bitmap.createBitmap(
-                        imageView.getWidth(),
-                        imageView.getHeight(),
-                        Bitmap.Config.ARGB_8888);
+                    imageView.getWidth(),
+                    imageView.getHeight(),
+                    Bitmap.Config.ARGB_8888);
                 canvas = new Canvas(bitmap);
-                doPaint();
+
+                startPaintAnimation();
             }
         });
+    }
+
+    private void startPaintAnimation()
+    {
+        Random r = new Random();
+        this.canvasColor = GetRandomColor();
+        this.positionY = r.nextInt(this.imageView.getWidth());
 
         ValueAnimator timerAnimator = ValueAnimator.ofFloat(0f, 1f);
-        timerAnimator.setDuration(TimeUnit.SECONDS.toMillis(5));
-        timerAnimator.setInterpolator(new LinearInterpolator());
+        timerAnimator.setDuration(TimeUnit.SECONDS.toMillis(1));
+        timerAnimator.setInterpolator(new AccelerateInterpolator());
+
         timerAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
                 progress = (float)animation.getAnimatedValue();
-                Log.d("foo", Float.toString(progress));
-                imageView.invalidate();
 
-                doPaint();
+                paintCanvas();
+
+                imageView.invalidate();
             }
         });
+
+        timerAnimator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                startPaintAnimation();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
         timerAnimator.start();
     }
 
-    private void doPaint()
+    private void paintCanvas()
     {
         if (canvas != null)
         {
-            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            paint.setColor(Color.MAGENTA);
-            canvas.drawCircle(100, imageView.getHeight() * progress, 20, paint);
+            Paint paint = new Paint();
+            paint.setColor(this.canvasColor);
+            //Log.d("foo", Float.toString(paint.getColor()));
+            Log.d("foo", Integer.toHexString(paint.getColor()));
+            canvas.drawCircle(this.positionY, this.imageView.getHeight() * this.progress, 20, paint);
             imageView.setImageBitmap(bitmap);
         }
+    }
+
+    private int GetRandomColor()
+    {
+        @ColorInt final int colors[] = new int[] {
+            0xFFFEDD12,
+            0xFFEE7523,
+            0xFFE72D92,
+            Color.parseColor("#23B2A8"),
+            Color.parseColor("#213468"),
+            0xFFFF80AB,
+            0xFFFF4081,
+            0xFFF50057,
+            0xFFC51162,
+            0xFF82B1FF,
+            0xFF448AFF,
+            0xFF2979FF,
+            0xFF2962FF
+        };
+        Random r = new Random();
+        return colors[r.nextInt(colors.length)];
     }
 }
