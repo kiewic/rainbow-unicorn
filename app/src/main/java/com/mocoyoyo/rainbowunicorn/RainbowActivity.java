857c04d96eccaffbecedbb0e559ebbfd3ca3e422
Gilberto Stankiewicz <kiewic@gmail.com>
2016-12-27 14:20:12 -0600

    Connect the circles with rectangles to make a smoothly drop effect.

--- app/src/main/java/com/mocoyoyo/rainbowunicorn/RainbowActivity.java
+++ app/src/main/java/com/mocoyoyo/rainbowunicorn/RainbowActivity.java
@@ -22,12 +22,15 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class RainbowActivity extends AppCompatActivity {
-    private int canvasColor = Color.MAGENTA;
+    private final float cicleRadius = 20;
+    private float lastPositionX = Float.NaN;
+    private float lastPositionY = Float.NaN;
     private float positionY;
     private float progress;
     private Bitmap bitmap;
     private Canvas canvas;
     private ImageView imageView;
+    private Paint paint;
 
 
     @Override
@@ -49,6 +52,7 @@ public class RainbowActivity extends AppCompatActivity {
                     imageView.getHeight(),
                     Bitmap.Config.ARGB_8888);
                 canvas = new Canvas(bitmap);
+                imageView.setImageBitmap(bitmap);
 
                 startPaintAnimation();
             }
@@ -57,8 +61,10 @@ public class RainbowActivity extends AppCompatActivity {
 
     private void startPaintAnimation()
     {
+        int canvasColor = GetRandomColor();
+        this.paint = new Paint();
+        this.paint.setColor(canvasColor);
         Random r = new Random();
-        this.canvasColor = GetRandomColor();
         this.positionY = r.nextInt(this.imageView.getWidth());
 
         ValueAnimator timerAnimator = ValueAnimator.ofFloat(0f, 1f);
@@ -104,31 +110,53 @@ public class RainbowActivity extends AppCompatActivity {
     {
         if (canvas != null)
         {
-            Paint paint = new Paint();
-            paint.setColor(this.canvasColor);
             //Log.d("foo", Float.toString(paint.getColor()));
-            Log.d("foo", Integer.toHexString(paint.getColor()));
-            canvas.drawCircle(this.positionY, this.imageView.getHeight() * this.progress, 20, paint);
-            imageView.setImageBitmap(bitmap);
+            float positionX = this.imageView.getHeight() * this.progress;
+
+            if (this.lastPositionY == this.positionY)
+            {
+                canvas.drawRect(
+                    this.positionY - this.cicleRadius,
+                    this.lastPositionX,
+                    this.positionY + this.cicleRadius,
+                    positionX,
+                    paint);
+            }
+
+            canvas.drawCircle(this.positionY, positionX, 20, this.paint);
+
+            this.lastPositionX = positionX;
+            this.lastPositionY = this.positionY;
         }
     }
 
     private int GetRandomColor()
     {
-        @ColorInt final int colors[] = new int[] {
+        @ColorInt final int colors[] = new int[]
+        {
+            // Yellow, Orange, Pink, Turquoise, Marine Blue
             0xFFFEDD12,
             0xFFEE7523,
             0xFFE72D92,
             Color.parseColor("#23B2A8"),
             Color.parseColor("#213468"),
+            // Pink
             0xFFFF80AB,
             0xFFFF4081,
             0xFFF50057,
             0xFFC51162,
+            // Blue
             0xFF82B1FF,
             0xFF448AFF,
             0xFF2979FF,
-            0xFF2962FF
+            0xFF2962FF,
+            // Green
+            0xFF00E676,
+            0xFF00C853,
+            // Yellow-Orange
+            0xFFFFD740,
+            0xFFFFC400,
+            0xFFFFAB00
         };
         Random r = new Random();
         return colors[r.nextInt(colors.length)];
